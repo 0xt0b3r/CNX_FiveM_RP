0098df7c92fd10c174c02bab0a371c70a2c1fa92
Roy DePhillip <rdephillip@gmail.com>
2019-01-04 19:11:33 -0500

    Dev Banner now completed and will show up for new connections!

--- testLabel/client.lua
+++ testLabel/client.lua
@@ -1,3 +1,38 @@
+local cancelBanner = false
+
+-- Check for resource load
+AddEventHandler('onClientResourceStart', function(resource) 
+
+    if GetCurrentResourceName() == resource then
+        TriggerServerEvent('InDev:Status')
+    end
+
+end)
+
+-- Start Drawing Banner
+RegisterNetEvent('InDev:Start')
+AddEventHandler('InDev:Start', function()
+    while true do
+        drawTxt(1.0, 1.0+0.42, 1.0,1.0,1.0, "| SERVER MAINTENANCE IN PROGRESS |", 255, 255, 0, 255)
+        
+        if cancelBanner then
+            cancelBanner = false
+            return
+        end
+
+        Citizen.Wait(0)
+    end
+end)
+
+-- Stop Drawing Banner
+RegisterNetEvent('InDev:Stop')
+AddEventHandler('InDev:Stop', function()
+
+    cancelBanner = true
+
+end)
+
+-- Draw Banner
 function drawTxt(x,y ,width,height,scale, text, r,g,b,a)
     SetTextFont(4)
     SetTextProportional(0)
@@ -10,31 +45,4 @@ function drawTxt(x,y ,width,height,scale, text, r,g,b,a)
     SetTextEntry("STRING")
     AddTextComponentString(text)
     DrawText(x - width/2, y - height/2 + 0.005)
-end
-
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(1)
-		if _bannerShow then
-			drawTxt(1.0, 1.0+0.42, 1.0,1.0,1.0, "| SERVER MAINTENANCE IN PROGRESS |", 255, 255, 0, 255)
-		end
-	end
-end)
-
--- toggle banner --
-RegisterNetEvent('testbanner')
-AddEventHandler('testbanner',function() 
-	if _bannerShow then
-		_bannerShow = false
-		drawNotification("SERVER TEST DISABLED")
-	else
-		_bannerShow = true
-		drawNotification("SERVER TEST ENABLED")
-	end
-end)
-
-function drawNotification(text)
-    SetNotificationTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawNotification(false, false)
 end
--- testLabel/server.lua
+++ testLabel/server.lua
@@ -1,10 +1,23 @@
-bannerShow = false
--- Toggle Test Banner --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/testbanner" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('testbanner', -1)
+local showBanner = false
+
+-- Toggle Test Banner
+RegisterCommand('indev', function(source, args, raw)
+	showBanner = not showBanner
+
+	if showBanner then
+		TriggerClientEvent('InDev:Start', -1)
+	else
+		TriggerClientEvent('InDev:Stop', -1)
 	end
+
+end, false)
+
+-- Check InDev Status
+RegisterServerEvent('InDev:Status')
+AddEventHandler('InDev:Status', function()
+
+	if showBanner then
+		TriggerClientEvent('InDev:Start', source)
+	end
+
 end)
